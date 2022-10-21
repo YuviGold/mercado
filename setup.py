5cafe2f69b609f8a497e2869f1299c45d4a563c8
Yuval Goldberg <yuvigoldi@gmail.com>
2022-10-21 15:23:42 +0300

    Add release workflow
    Resolves #4

--- setup.py
+++ setup.py
@@ -6,11 +6,13 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(name='mercado',
-      version='0.1',
+      version='0.1.1',
       description='Open Source market for utilities',
       author='YuviGold',
+      url='https://github.com/YuviGold/mercado/',
       packages=find_packages(),
       install_requires=requirements,
+      setup_requires=['wheel'],
       entry_points={
           'console_scripts': ['mercado=mercado.cli:main']
       }
