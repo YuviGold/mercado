1ef5346c023b56a749f69241f27cad1de0c01bd3
Yuval Goldberg <yuvigoldi@gmail.com>
2022-11-30 12:51:01 +0200

    Promote version to 0.1.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(name='mercado',
-      version='0.1.1',
+      version='0.1.2',
       description='Open Source market for utilities',
       author='YuviGold',
       url='https://github.com/YuviGold/mercado/',
